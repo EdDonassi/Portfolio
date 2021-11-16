d13a9a6528d0d361bc019f1fbe2fcc280973c881
EdDonassi <moha_ed@hotmail.es>
2021-11-16 22:14:54 +0100

    updated js

--- ressources/javascript/app.js
+++ ressources/javascript/app.js
@@ -1,13 +1,7 @@
 let navLinks = document.querySelector('.links');
 let menuIcon = document.querySelector('#menu-icon');
-let links = document.getElementById('#links');
 
 menuIcon.addEventListener('click', function () {
     navLinks.classList.toggle('is-active');
 })
 
-links.forEach(sec => {
-    sec.addEventListener('click', func => {
-        sec.classList.add('active');
-    })
-});
