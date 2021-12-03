dbc0634a8a1e8bb6445dfe177382210cbffeb13c
EdDonassi <87482442+EdDonassi@users.noreply.github.com>
2021-12-03 14:13:29 +0100

    Delete ressources directory

--- ressources/javascript/app.js
+++
@@ -1,7 +0,0 @@
-let navLinks = document.querySelector('.links');
-let menuIcon = document.querySelector('#menu-icon');
-
-menuIcon.addEventListener('click', function () {
-    navLinks.classList.toggle('is-active');
-})
-
